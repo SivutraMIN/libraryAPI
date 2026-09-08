9511106daca82723922bbe96aa67150caf924e30
SivutraMIN <minsivutra@gmail.com>
2026-09-08 18:18:31 +0700

    fixing a lot of importing stuff

--- app/database/database.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel,create_engine
-from . import models # type: ignore
-
-sqlite_file_name = "db.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = create_engine(sqlite_url,echo=True)
-
-
-SQLModel.metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,39 @@
 from fastapi import FastAPI
-from router.books import router as books
-from router.admin import router as admin
+from sqlmodel import SQLModel,create_engine,Field, Session, select
+
+class Student(SQLModel, table=True):
+    id: int  | None = Field(default=None, primary_key=True)
+    name: str
+    age: int
 
 
+
+sqlite_file_name = "db.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url,echo=True)
+SQLModel.metadata.create_all(engine)
 
 
 app = FastAPI()
 
-app.include_router(books)
-app.include_router(admin)
+@app.get("/")
+def root():
+    return {"Message":"Welcome to our Library"}
+
+@app.post("/createStudent")
+def create_user():
+    with Session(engine) as session:
+        statement = Student(name="John", age="20")
+        session.add(statement)
+        session.commit()
+
+@app.get("/user")
+def get_all_user():
+    with Session(engine) as session:
+        statement = select(Student)
+        
+        users = session.exec(statement).all()
+
+        return users
+
