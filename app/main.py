7b254c232c820f631dd56484330ab5fe2fb217f1
SivutraMIN <minsivutra@gmail.com>
2026-09-09 22:32:07 +0700

    LEAKED ENV FILE, HAHAH

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ class Student(SQLModel, table=True):
     id: int  | None = Field(default=None, primary_key=True)
     name: str
     age: int
+    year: int
 
 
 
 sqlite_file_name = "db.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_url = f"sqlite:///app/database/{sqlite_file_name}"
 
 engine = create_engine(sqlite_url,echo=True)
 SQLModel.metadata.create_all(engine)
@@ -21,19 +22,43 @@ app = FastAPI()
 def root():
     return {"Message":"Welcome to our Library"}
 
+#User
+""" It has
++ Creating a User
++ Reading a User
++ Updating a User
++ Deleting a User
+"""
+# Creating User
 @app.post("/createStudent")
 def create_user():
     with Session(engine) as session:
-        statement = Student(name="John", age="20")
+        statement = Student(name="Jake", age="50", year="5")
         session.add(statement)
         session.commit()
+
+        return statement
 
 @app.get("/user")
 def get_all_user():
     with Session(engine) as session:
         statement = select(Student)
-        
         users = session.exec(statement).all()
-
         return users
 
+@app.put("/recreateUser")
+def recreateUser():
+    ...
+
+@app.patch("/updateUser")
+def updateUser():
+    ...
+
+
+#Books
+"""It has
++ Creating a Book
++ Reading a Book
++ Updating a Book
++ Deleting a Book
+"""
